51fc95cba7041f49abc24cf4cd7a8e45f208ab8f
nadhir <Mon Pc@DESKTOP-IHFN71P>
2022-10-18 02:03:40 +0100

    update test junit

---
+++ src/test/java/com/esprit/examen/controllers/CategorieProduitControllerTest.java
@@ -0,0 +1,34 @@
+package com.esprit.examen.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategorieProduitControllerTest {
+
+	@Test
+	void testGetCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetrieveCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testModifyCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+}
